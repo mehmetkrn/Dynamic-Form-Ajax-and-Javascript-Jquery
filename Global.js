0f195d894b4e8b4fbc8f03fdab818afe8d5a65bc
Mehmet Kıran <57233999+mehmetkrn@users.noreply.github.com>
2025-07-03 09:36:41 +0300

    Update Global.js
    -Dönüşlerde Başarılı mesajının çıkması eklendi.
    -Yönlendirme yapılabilmesi eklendi.
    -Hataların ekrandaki bir alana yazılması eklendi.
    -Bir işlem içerisinde başka bir işlem çalışması ve içine değer alması eklendi (CallBack)

--- Global.js
+++ Global.js
@@ -1,18 +1,14 @@
-async function AjaxFormSubmit(form, callback) {
-    //data-Yonlenecek attr si gidiceği url verilmeli
-    //data-Refresh attr True yada false verilmeli eğer sayfa kendi içinde refres edilicekse
-    //data-IsRespons attr True yada false olmalı sayfa yönlenicek ise true ynlenmeyecek ise false olmalı
-
+async function AjaxFormSubmit(form, callback) { 
 
     form.find("button[type=\"submit\"]").attr("disabled", true);
     var id = form.attr("id")
-    var Url = form.attr("data-Yonlenecek")
+
     var Refresh = form.attr("data-Refresh")
     var IsRespons = form.attr("data-IsRespons")
     var Datas = $("#" + id + " :input")
     var formData = new FormData();
     for (var i = 0; i < Datas.length; i++) {
-        
+
         switch (Datas[i].type) {
             case "file":
                 for (var j = 0; j < Datas[i].files.length; j++) {
@@ -21,6 +17,12 @@ async function AjaxFormSubmit(form, callback) {
                 break;
             case "checkbox":
                 formData.append(Datas[i].name, Datas[i].checked);
+                break;
+            case "radio":
+                console.log(Datas[i].checked)
+                if (Datas[i].checked) {
+                    formData.append(Datas[i].name, Datas[i].value);
+                }
                 break;
             case "select-multiple":
                 var datas = $("[name='" + Datas[i].name + "']").select2("data");
@@ -37,8 +39,6 @@ async function AjaxFormSubmit(form, callback) {
                 formData.append(Datas[i].name, Datas[i].value);
                 break;
         }
-
-
     }
     $.ajax({
         type: form[0].method,
@@ -47,67 +47,69 @@ async function AjaxFormSubmit(form, callback) {
         processData: false,
         contentType: false
     }).done(async function (response) {
-        await mesajGoster("Success", true, false);
-        if (IsRespons == "True") {
-            if (Refresh == "True") {
-                window.location.reload();
-            }
-            else {
-                await yonlendir(Url + "/" + response);
-            }
-            return false;
-        }
-        else {
+        response = JSON.parse(response)
+        if (response.IsSuccess) {
+            await Success();
             if (callback) {
-                callback();
+                callback(response);
             }
             else {
-                return response;
+                if (response.ResultData == "") {
+                    window.location.reload();
+                }
+                else {
+                      forward(response.ResultData);
+                }
             }
-            form.find("button[type=\"submit\"]").attr("disabled", false);
 
         }
-        console.log(response);
+        else {
+            ErrorMessageBoxCreator(response)
+        }
+        form.find("button[type=\"submit\"]").attr("disabled", false);
+        document.body.removeAttribute("style");
         return false;
 
     }).fail(function (data) {
         console.log(data);
         alert("Console Log");
+        document.body.removeAttribute("style");
     });
 }
 
-async function GetFormData(FormId) { 
-    var id = FormId 
-    var Datas = $("#" + id + " :input")
-    var formData = new FormData();
-    for (var i = 0; i < Datas.length; i++) {
+function ErrorMessageBoxCreator(data) {
+    document.getElementById(data.ErrorDivId + "Box").style.display = "block";
+    document.getElementById(data.ErrorDivId).innerHTML = "";
+    var Messages = "";
 
-        switch (Datas[i].type) {
-            case "file":
-                for (var j = 0; j < Datas[i].files.length; j++) {
-                    formData.append(Datas[i].name, Datas[i].files[j]);
-                }
-                break;
-            case "checkbox":
-                formData.append(Datas[i].name, Datas[i].checked);
-                break;
-            case "select-multiple":
-                var datas = $("[name='" + Datas[i].name + "']").select2("data");
-                var arrystr = "";
-                for (var j = 0; j < datas.length; j++) {
-                    arrystr += datas[j].id + ",";
-                };
-                if (arrystr.length > 0)
-                    arrystr = arrystr.substring(0, arrystr.length - 1)
-                formData.append(Datas[i].name, arrystr);
-                break;
-
-            default:
-                formData.append(Datas[i].name, Datas[i].value);
-                break;
-        }
-
-
+    for (var i = 0; i < data.ErrorMessages.length; i++) {
+        Messages += " <div>*" + data.ErrorMessages[i] + "</div>";
     }
-    return formData;
+
+    document.getElementById(data.ErrorDivId).innerHTML = Messages
+    document.getElementById(data.ErrorDivId + "Box").scrollIntoView();
+}
+function MessageHide() {
+    document.getElementById("dvErrorBox").style.display = "none";
+
+}
+
+function Success() {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'İşlem Başarılı',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    document.body.removeAttribute("style");
+}
+
+  function forward(url) {
+    setTimeout(function () {
+
+        window.location.href = url;
+
+    }, 2000);
+
 }
